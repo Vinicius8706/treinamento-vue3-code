7a22e7ce2050f8ae46648bfaa699edf22372c65f
Vinicius <fsvinicius9@gmail.com>
2022-05-30 22:07:54 -0300

    Ajeitando o router

--- dashboard/src/main.js
+++ dashboard/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
-createApp(App).use(router).mount('#app')
+import '@/assets/css/tailwind.css'
+import '@/assets/css/fonts.css'
+import 'animate.css'
+const app = createApp(App).use(router).mount('#app')
